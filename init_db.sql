9d1d117755b60589d2f12c3a3f4554bd90e756bf
Oleg Fedorovich <6x7missu7x7@gmail.com>
2019-09-27 18:28:07 +0300

    fix audio table and remove author

--- init_db.sql
+++ init_db.sql
@@ -2,16 +2,9 @@ CREATE DATABASE goass;
 
 USE goass;
 
-CREATE TABLE authors (
-    id BIGINT NOT NULL AUTO AUTO_INCREMENT,
-    name NVARCHAR(50) NOT NULL UNIQUE,
-    PRIMARY KEY(id),
-    CHECK(name <> "")
-);
-
 CREATE TABLE audio (
     id BIGINT NOT NULL AUTO_INCREMENT,
-    author NVARCHAR(50) NOT_NULL,
+    author NVARCHAR(50) NOT NULL,
     title NVARCHAR(50) NOT NULL,
     PRIMARY KEY(id),
     CHECK(author <> ""),
