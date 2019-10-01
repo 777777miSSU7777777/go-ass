8308b279f1e1b7225938150ba56fec7d22e5ae74
Oleg Fedorovich <6x7missu7x7@gmail.com>
2019-10-01 17:46:23 +0300

    remove travis ci intergration for a while

--- docker_push.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-docker build -t $SERVICE_TAG:$TRAVIS_TAG .
-
-echo "$DOCKER_PASSWORD" | docker login --username $DOCKER_LOGIN --password-stdin;
-
-docker tag $SERVICE_TAG:$TRAVIS_TAG $DOCKER_LOGIN/$SERVICE_TAG:$TRAVIS_TAG;
-
-docker push $DOCKER_LOGIN/$SERVICE_TAG:$TRAVIS_TAG;
