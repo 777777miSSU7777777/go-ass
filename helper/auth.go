c768125549a4288bbd57e57fa8f9f522217a124b
777777miSSU7777777 <6x7missu7x7@gmail.com>
2020-09-15 19:44:21 +0300

    add auth helper

---
+++ helper/auth.go
@@ -0,0 +1,19 @@
+package helper
+
+import (
+	"fmt"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+const UserRole = "user"
+
+func HashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	return string(bytes), err
+}
+
+func CheckPasswordHash(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
+}
