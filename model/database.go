bb4900134335bebb4e8270eb855b5bc06cc546f2
777777miSSU7777777 <o.fedorovich@yumasoft.com>
2020-10-12 19:33:34 +0300

    fix db models

--- model/database.go
+++ model/database.go
@@ -1,7 +1,7 @@
 package model
 
 type Artist struct {
-	ArtistID   string `gorm:"column:artist_id;type:uuid;primaryKey;autoIncrement;unique;not null"`
+	ArtistID   string `gorm:"column:artist_id;type:uuid;primaryKey;unique;not null"`
 	ArtistName string `gorm:"column:artist_name;type:varchar(50);unique;not null"`
 }
 
@@ -11,7 +11,7 @@ type GenreTracks struct {
 }
 
 type Genre struct {
-	GenreID    string `gorm:"column:genre_id;type:uuid;primaryKey;autoIncrement;unique; not null"`
+	GenreID    string `gorm:"column:genre_id;type:uuid;primaryKey;unique;not null"`
 	GenreTitle string `gorm:"column:genre_title;type:varchar(50);unique;not null"`
 }
 
@@ -21,36 +21,36 @@ type PlaylistTracks struct {
 }
 
 type Playlist struct {
-	PlaylistID    string `gorm:"column:playlist_id;type:uuid;primaryKey;autoIncrement;unique;not null"`
+	PlaylistID    string `gorm:"column:playlist_id;type:uuid;primaryKey;unique;not null"`
 	PlaylistTitle string `gorm:"column:playlist_title;type:varchar(50);not null"`
 	CreatedByID   string `gorm:"column:created_by_id;type:uuid;not null"`
 }
 
 type Track struct {
-	TrackID      string `gorm:"column:track_id;type:uuid;primaryKey;autoIncrement;unique;not null"`
+	TrackID      string `gorm:"column:track_id;type:uuid;primaryKey;unique;not null"`
 	TrackTitle   string `gorm:"column:track_title;type:varchar(50);not null"`
 	ArtistID     string `gorm:"column:artist_id;type:uuid;not null"`
-	GenreID      string `gorm:"column:genre_id;type:uuid; not null"`
+	GenreID      string `gorm:"column:genre_id;type:uuid;not null"`
 	UploadedByID string `gorm:"column:uploaded_by_id;type:uuid;not null"`
 }
 
 type UserPlaylists struct {
 	UserID     string `gorm:"column:user_id;type:uuid;primaryKey;not null"`
-	PlaylistID string `gorm:"column:playlist_id type:uuid;primaryKey;not null"`
+	PlaylistID string `gorm:"column:playlist_id;type:uuid;primaryKey;not null"`
 }
 
 type UserTokens struct {
-	UserID string `gorm:"type:uuid;primaryKey;not null"`
-	Token  string `gorm:"type:text;primaryKey;not null"`
+	UserID string `gorm:"column:user_id;type:uuid;primaryKey;not null"`
+	Token  string `gorm:"coulmn:token;type:text;primaryKey;not null"`
 }
 
 type UserTracks struct {
-	UserID  string `gorm:"type:uuid;primaryKey;not null"`
-	TrackID string `gorm:"type:uuid;primaryKey;not null"`
+	UserID  string `gorm:"column:user_id;type:uuid;primaryKey;not null"`
+	TrackID string `gorm:"coulmn:track_id;type:uuid;primaryKey;not null"`
 }
 
 type User struct {
-	UserID   string `gorm:"column:user_id;type:uuid;primaryKey;autoIncrement;unique;not null"`
+	UserID   string `gorm:"column:user_id;type:uuid;primaryKey;unique;not null"`
 	Role     string `gorm:"column:role;type:varchar(50);not null;"`
 	Email    string `gorm:"column:email;type:varchar(50);unique;not null"`
 	Username string `gorm:"column:username;type:varchar(50);unique;not null"`
